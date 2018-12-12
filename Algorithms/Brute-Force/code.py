85c8b1139294dfbe5bc04fefe4f3337579495786
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2018-12-12 18:26:18 +0530

    Create code.py

---
+++ Algorithms/Brute-Force/code.py
@@ -0,0 +1,25 @@
+
+
+p =(2**80)
+print(p)
+
+a = 5
+print(a)
+
+g = 3
+count = 0
+
+for i in range(2, p-2):
+	if( a == (g**i)):
+		print("ATTACK DONE")
+	else:
+		count = count + 1
+		
+print(count)
+		
+		
+		
+		
+		
+		
+		
